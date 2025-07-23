de4ce4d0471d918d397ff968a3f4c82edac21b67
Zulkifal05 <zulkifalali299@gmail.com>
2025-07-23 19:33:56 +0500

    Routing Started

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import React from 'react'
-import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import  Login  from "./pages/Login"
-import CreatePost from './pages/CreatePost'
-import EditPost from "./pages/EditPost"
-import MyPosts from './pages/MyPosts'
-import SinglePostPreview from './pages/SinglePostPreview'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
 
   return (
     <>
       <Header />
-      {/* <SinglePostPreview /> */}
-      {/* <EditPost /> */}
-      {/* <Home /> */}
-      {/* <MyPosts /> */}
-      {/* <Login /> */}
-      {/* <CreatePost /> */}
-      {/* <Login /> */}
+      <Outlet />
       <Footer />
     </>
   )
